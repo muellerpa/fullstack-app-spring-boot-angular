26f318ea00f48ae12c751a6687e86303da2cbd48
Patrick <Patrick@DESKTOP-9I14EKK>
2022-02-28 16:18:04 +0100

    Initial commit

---
+++ src/main/java/fullstackappspringbootangular/FullstackAppSpringBootAngularApplication.java
@@ -0,0 +1,14 @@
+package fullstackappspringbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackAppSpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackAppSpringBootAngularApplication.class, args);
+		System.out.print("Test");
+	}
+
+}
---
+++ src/test/java/fullstackappspringbootangular/FullstackAppSpringBootAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package fullstackappspringbootangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackAppSpringBootAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
